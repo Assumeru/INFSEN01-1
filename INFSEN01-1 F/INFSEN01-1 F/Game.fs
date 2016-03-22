2be252fc4e597dfb0c483af8495bc60f336fab69
Assumeru <malusluminis@hotmail.com>
2016-03-22 16:12:41 +0100

    look look

--- INFSEN01-1 F/INFSEN01-1 F/Game.fs
+++ INFSEN01-1 F/INFSEN01-1 F/Game.fs
@@ -1,8 +1,15 @@
 ﻿module Game
 
+type Direction =
+    | north = 0
+    | east = 1
+    | south = 2
+    | west = 3
+
 type Object = {
     x: int;
     y: int;
+    r: Direction
 }
 
 type State = {
@@ -12,26 +19,36 @@ type State = {
     paused: bool;
 }
 
+let toString(dir) =
+    match dir with
+    | Direction.north -> "north"
+    | Direction.east -> "east"
+    | Direction.south -> "south"
+    | Direction.west -> "west"
+
+let tileExists(x, y, state) = 
+    y >= 0 && y < state.map.Length && x >= 0 && x < state.map.[y].Length
+
 let moveXY (dir, x, y, state) =
     let newX = state.player.x + x
     let newY = state.player.y + y
-    if newY < 0 || newY >= state.map.Length || newX < 0 || newX >= state.map.[newY].Length then
-        ("You cannot go that way", state)
-    else
+    if tileExists(newX, newY, state) then
         let tile = state.map.[newY].[newX]
         match tile with
-        | 'o' -> ("You moved " + dir, {state with player = {x = newX; y = newY;};})
+        | 'o' -> ("You moved " + toString(dir), {state with player = {state.player with x = newX; y = newY; r = dir};})
         | _ -> ("Your path is blocked", state)
+    else
+        ("You cannot go that way", state)
 
+let applyDir(dir, state, func) =
+    match dir with
+    | Direction.north -> func(dir, 0, -1, state)
+    | Direction.east -> func(dir, 1, 0, state)
+    | Direction.south -> func(dir, 0, 1, state)
+    | Direction.west -> func(dir, -1, 0, state)
 
 let move (dir, state) =
-    match dir with
-    | "north" -> moveXY(dir, 0, -1, state)
-    | "east" -> moveXY(dir, 1, 0, state)
-    | "south" -> moveXY(dir, 0, 1, state)
-    | "west" -> moveXY(dir, -1, 0, state)
-
-
+    applyDir(dir, state, moveXY)
 
 let checkSurrounding (dir, x, y, state) =
     let x = state.player.x 
@@ -41,31 +58,56 @@ let checkSurrounding (dir, x, y, state) =
     else
         "You can move to " + dir, state
 
-let canMoveTo(dir, state) = 
-    match dir with
-    | "north" -> checkSurrounding(dir, 0, -1, state)
-    | "east" -> checkSurrounding(dir, 1, 0, state)
-    | "south" -> checkSurrounding(dir, 0, 1, state)
-    | "west" -> checkSurrounding(dir, -1, 0, state)
-    | _ -> ("Unknown command", state)
+let gazeAt(tile) =
+    match tile with
+    | 'o' -> "An empty corridor"
+    | 'c' -> "A wall"
+    | _ -> "Something unknown"
 
+let lookXY (dir, x, y, state) =
+    let x = state.player.x + x
+    let y = state.player.y + y
+    if tileExists(x, y, state) then
+        gazeAt(state.map.[y].[x])
+    else
+        "The end of the world"
+
+let look(dir, state) =
+    applyDir(dir, state, lookXY)
+
+let rec lookList(dirs, state) : string =
+    match dirs with
+    | [] -> ""
+    | x :: y -> "To the " + toString x + ": " + look(x, state) + "\n" + lookList(y, state)
 
 let lookAround (state) = 
-    let positionsList = ["north";"east";"south";"west"]
-    for i in positionsList do
-       canMoveTo(i, state)
+    let positionsList = [Direction.north;Direction.east;Direction.south;Direction.west]
+    lookList(positionsList, state)
 
-
+let getDirection(dir, state) =
+    match dir with
+    | "left" -> enum<Direction>((int state.player.r + 3) % 4)
+    | "right" -> enum<Direction>((int state.player.r + 1) % 4)
+    | "behind" -> enum<Direction>((int state.player.r + 2) % 4)
+    | _ -> state.player.r
 
 let parseCommand (x, state : State) =
     match x with
     | "stop" -> ("Bye", {state with running = false})
-    | "north" | "east" | "south" | "west" -> move(x, state)
-    | "lookaround" -> lookAround(state)
+    | "north" -> move(Direction.north, state)
+    | "east" -> move(Direction.east, state)
+    | "south" -> move(Direction.south, state)
+    | "west" -> move(Direction.west, state)
+    | "look around" -> lookAround(state), state
+    | "look left" -> look(getDirection("left", state), state), state
+    | "look right" -> look(getDirection("right", state), state), state
+    | "look behind" -> look(getDirection("behind", state), state), state
+    | "look ahead" -> look(getDirection("ahead", state), state), state
+    | "turn left" -> "You turned left", {state with player = {state.player with r = getDirection("left", state)}}
+    | "turn right" -> "You turned right", {state with player = {state.player with r = getDirection("right", state)}}
+    | "turn around" -> "You turned around", {state with player = {state.player with r = getDirection("behind", state)}}
     | _ -> ("Unknown command", state)
 
 let runFrame (state: State) =
     printfn "(%d, %d)" state.player.x state.player.y
     state
-
-
